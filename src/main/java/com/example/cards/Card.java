bd2b864e639e22d9cff0178bc924334d703dad1f
Thomas <thomasbarkats@gmail.com>
2019-07-10 12:06:33 +0200

    card object impl

--- src/main/java/com/example/cards/Card.java
+++ src/main/java/com/example/cards/Card.java
@@ -2,7 +2,37 @@ package com.example.cards;
 
 public class Card {
 
-    public Card() {
+    private Symbols symbol;
+    private Colors color;
+    private CardValues value;
 
+    public Card(Symbols symbol, Colors color, CardValues value) {
+        this.symbol = symbol;
+        this.color = color;
+        this.value = value;
+    }
+
+    public Symbols getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(Symbols symbol) {
+        this.symbol = symbol;
+    }
+
+    public Colors getColor() {
+        return color;
+    }
+
+    public void setColor(Colors color) {
+        this.color = color;
+    }
+
+    public CardValues getValue() {
+        return value;
+    }
+
+    public void setValue(CardValues value) {
+        this.value = value;
     }
 }
---
+++ src/main/java/com/example/cards/CardValues.java
@@ -0,0 +1,17 @@
+package com.example.cards;
+
+public enum CardValues {
+    N2,
+    N3,
+    N4,
+    N5,
+    N6,
+    N7,
+    N8,
+    N9,
+    N10,
+    VALET,
+    DAME,
+    ROI,
+    AS;
+}
