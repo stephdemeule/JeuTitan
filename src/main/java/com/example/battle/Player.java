b043cf2e895c74b22f2d638b7f5e8848dfce78dd
RegisPP <44397206+RegisPP@users.noreply.github.com>
2019-07-10 14:58:33 +0200

    create player object

--- src/main/java/com/example/battle/Player.java
+++ src/main/java/com/example/battle/Player.java
@@ -1,8 +1,42 @@
 package com.example.battle;
+import com.example.cards.Card;
+
+import java.util.List;
 
 public class Player {
 
-    public Player() {
+    private String nom;
+    private List<Card> cards;
 
+
+    public Player(String nom, List<Card> cards) {
+        this.nom = nom;
+        this.cards = cards;
+    }
+
+    public void giveCard(Card card){
+        cards.add(card);
+    }
+
+    public void giveCards(List<Card> cards){
+        for (Card card : cards){
+            cards.add(card);
+        }
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
     }
 }
