252ded157014f4547e03055202a3f8593adb5195
Thomas <thomasbarkats@gmail.com>
2019-07-10 17:31:28 +0200

    test integration

---
+++ src/test/java/com/example/battle/CardPackServiceTest.java
@@ -0,0 +1,20 @@
+package com.example.battle;
+
+import com.example.cards.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CardPackServiceTest {
+
+    @Test
+    void createClassicPack() {
+        final com.example.cards.CardPackService cardPackService = new CardPackServiceImpl();
+        final int expectedNbCards = 52;
+
+        CardPack cardPack = cardPackService.createClassicPack();
+        int nbCards = cardPack.getCards().size();
+
+        assertEquals(expectedNbCards, nbCards);
+    }
+}
